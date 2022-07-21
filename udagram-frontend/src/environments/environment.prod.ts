3f7d4c693d47775080ddeabeb33c70aee8c47ad1
nobleman97 <davidstone097@gmail.com>
2022-07-21 12:26:51 +0100

    add API to frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab9fd59e1baa7498588094042686b8ed-1739427273.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
